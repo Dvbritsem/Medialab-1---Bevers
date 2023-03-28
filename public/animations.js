7724470b44776ccbd859e47d8de7af9b907c61c7
Dennis van Britsem <britsem@bubblefish.agency>
2023-03-28 14:04:05 +0200

    Added code from Thomas

---
+++ public/animations.js
@@ -0,0 +1,7 @@
+function startAnimation() {
+    document.getElementById("laadwiel").classList.remove('invisible');
+}
+
+function stopAnimation() {
+    document.getElementById("laadwiel").classList.add('invisible');
+}
